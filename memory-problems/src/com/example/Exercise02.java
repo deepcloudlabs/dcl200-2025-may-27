120d73f1a4b8256b9cf2c6b35cd3105fae9e5dae
Binnur KURT <deepcloudlabs@gmail.com>
2025-05-28 16:37:04 +0300

    initial commit

---
+++ memory-problems/src/com/example/Exercise02.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Exercise02 {
+
+	public static int fun() {
+		try {
+			return 100;
+		} finally {
+			return 200;
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println(fun());
+	}
+
+}
